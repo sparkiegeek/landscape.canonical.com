193a15386d208d56f5b68545fa6d0617f614ee85
Richard McCartney <richard@mccartney.io>
2015-12-10 11:35:52 +0000

    Adding main.scss

--- config.rb
+++ config.rb
@@ -3,10 +3,10 @@ require 'compass/import-once/activate'
 
 # Set this to the root of your project when deployed:
 http_path = "/"
-css_dir = "static/css"
-sass_dir = "static/sass"
-images_dir = "static/img"
-javascripts_dir = "static/js"
+css_dir = "/static/css"
+sass_dir = "/static/scss"
+images_dir = "/static/img"
+javascripts_dir = "/static/js"
 
 # You can select your preferred output style here (can be overridden via the command line):
 output_style = :compressed #:expanded or :nested or :compact or :compressed
