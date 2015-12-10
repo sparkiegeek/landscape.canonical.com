315732da3248e51fefd9ea1e993800bb314ace83
Richard McCartney <richard@mccartney.io>
2015-12-10 11:13:38 +0000

    Adding compass config settings

--- config.rb
+++ config.rb
@@ -3,13 +3,13 @@ require 'compass/import-once/activate'
 
 # Set this to the root of your project when deployed:
 http_path = "/"
-css_dir = "stylesheets"
-sass_dir = "sass"
-images_dir = "images"
-javascripts_dir = "javascripts"
+css_dir = "static/css"
+sass_dir = "static/sass"
+images_dir = "static/img"
+javascripts_dir = "static/js"
 
 # You can select your preferred output style here (can be overridden via the command line):
-# output_style = :expanded or :nested or :compact or :compressed
+output_style = :compressed #:expanded or :nested or :compact or :compressed
 
 # To enable relative paths to assets via compass helper functions. Uncomment:
 # relative_assets = true
